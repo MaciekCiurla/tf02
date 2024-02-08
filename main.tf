504c74c9fa839e84da48e65d438ea4e11ef29e87
MaciekCiurla <93926926+MaciekCiurla@users.noreply.github.com>
2024-02-09 00:12:01 +0100

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,25 @@
+# script creates multiple RGs based on a given count using "count" structure
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
+
+variable "vm_count" {
+  type = number
+  default = 3
+}
+
+resource "azurerm_resource_group" "server_rg" {
+  count = var.vm_count
+  name     = "RG-MC-${count.index}"
+  location = "westeurope"
+}
